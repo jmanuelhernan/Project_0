1421e651f6aa5177f861455258ed07f3532da556
jmanuelhernan <jm.hernandezs1@uniandes.edu.co>
2025-02-16 11:32:02 -0500

    Proyecto en espera de prueba

--- logic.py
+++ logic.py
@@ -5,7 +5,7 @@ data_dir = os.path.dirname(os.path.realpath(__file__))
 def abrir_fichero():
     nombre=str(input("Ingrese el nombre del archivo (debe estar en la misma carpeta que los .py): "))
     ruta = os.path.join(data_dir,nombre)
-    print("Buscando archivo en: ".format(ruta) )
+    print("Buscando archivo en: ".format(ruta))
     try:
         with open(ruta, "r") as fichero:
             return fichero.readlines()
@@ -21,7 +21,8 @@ class RobotRecognizer:
         self.procedures = {}
         self.errors = []
         self.call_stack = []
-        pass
+        self.instructions = ["move","turn","face","goto","put","pick","jump","if","while","proc"]
+        
 
     def separador (self,line):
         caracteres = ""
@@ -39,12 +40,62 @@ class RobotRecognizer:
         if caracteres:
             lista_caracteres.append(caracteres)
         return lista_caracteres
+    
     def parse(self):
         if not self.lines:
-            return
+            return False
         for line in self.lines:
             caracteres= self.separador(line.strip())
-            print ("Caracteres en linea: {}".format(caracteres))
+            self.procesador(caracteres)
+        return len(self.errors) == 0
+
+    def procesador(self, caracteres):
+        if not caracteres:
+            return
+        comandos = caracteres[0]
+        if comandos in self.instructions:
+            self.validador(comandos, caracteres[1:])
+        else: 
+            self.errors.append("Comando desconocido: {}".format(comandos))
+    
+    def validador(self, comandos, args):
+        if comandos == "goto":
+            if len(args) == 3 and args[1] == "with:" and args[0].isdigit() and args[2].isdigit():
+                return
+            else:
+                self.errors.append("Error en el comando goto")
+        elif comandos == "move":
+            if not args[0].isdigit():
+                self.errors.append("Error en el comando move")
+        elif comandos == "turn":
+            if args[0] not in ["#left","#right","#around"]:
+                self.errors.append("Error en el comando turn")
+        elif comandos == "face":
+            if args[0] not in ["#north","#south","#west","#east"]:
+                self.errors.append("Error en el comando face")
+        elif comandos == "put":
+            if len(args) == 3 and args[1] == "ofType:" and args[0].isdigit() and args[2] in ["#baloons", "#chips"]:
+                return
+            else:
+                self.errors.append("Error en el comando put")
+        elif comandos == "pick":
+            if len(args) == 3 and args[1] == "ofType:" and args[0].isdigit() and args[2] in ["#baloons", "#chips"]:
+                return
+            else:
+                self.errors.append("Error en el comando pick")
+        elif comandos == "jump":
+            if len(args) == 3 and (args[1] == "toThe:" or args[1] == "inDir:") and args[0].isdigit() and args[2] in ["#front", "#right","#left","#back","#north", "#south","#west","#east"]:
+                return
+            else:
+                self.errors.append("Error en el comando jump")
+                
+    def errores(self):
+        if self.errors:
+            print("Errores encontrados en el lenguaje: ")
+            for errores in self.errors:
+                print(errores)
+        else: 
+            print("El lenguaje es correcto.")
         
 def reconocedor ():
     archivo=abrir_fichero()
@@ -54,5 +105,7 @@ def reconocedor ():
     else:
         robot = RobotRecognizer(archivo)
         
-        robot.parse()
-    
+        resultado = robot.parse()
+        robot.errores()
+        return resultado
+
